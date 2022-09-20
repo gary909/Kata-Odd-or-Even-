35967c5f90ef82c3d15be0477be0ec7a9de461af
gary909 <garywhite61@hotmail.com>
2022-09-20 12:49:14 +0200

    code

---
+++ app.js
@@ -0,0 +1,8 @@
+function oddOrEven(array) {
+    //enter code here
+}
+
+console.log(oddOrEven([0])); // even
+console.log(oddOrEven([1])); // odd
+console.log(oddOrEven([])); // even
+console.log(oddOrEven([0, 1, 5])); // even
