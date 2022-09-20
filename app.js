e0da73c119b6937753db98e9401dab455db24288
gary909 <garywhite61@hotmail.com>
2022-09-20 13:00:48 +0200

    code working

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 function oddOrEven(array) {
-    //enter code here
+    // sum the array, check if empty
+    let mySum = array.reduce(add, 0);
+
+    function add(accumulator, a) {
+        return accumulator + a;
+    }
+
+    if (mySum % 2 == 0) {
+        return 'even';
+    } else {
+        return 'odd';
+    }
 }
 
 console.log(oddOrEven([0])); // even
 console.log(oddOrEven([1])); // odd
 console.log(oddOrEven([])); // even
 console.log(oddOrEven([0, 1, 5])); // even
+console.log(oddOrEven([0, -1, -5])); // even
